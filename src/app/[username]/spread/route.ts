f7b300c32fa829634a281d61ee89809c3e44b370
Lucas Maciel <lucasmacielcontato@gmail.com>
2024-05-21 20:00:15 -0300

    Fix: Preparing for deploy (test)

--- src/app/[username]/spread/route.ts
+++ src/app/[username]/spread/route.ts
@@ -1,12 +1,11 @@
-import type { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request, res: Response) {
   return new NextResponse(
     `
-    <svg width="200" height="150" viewBox="0 0 200 150" xmlns="http://www.w3.org/2000/svg">
-      <ellipse cx="100" cy="75" rx="80" ry="60" fill="#f00" stroke="#000" stroke-width="2"/>
-    </svg>
+      <svg width="200" height="150" viewBox="0 0 200 150" xmlns="http://www.w3.org/2000/svg">
+        <ellipse cx="100" cy="75" rx="80" ry="60" fill="#f00" stroke="#000" stroke-width="2"/>
+      </svg>
     `,
     { status: 410, headers: { "content-type": "image/svg+xml" } }
   );
